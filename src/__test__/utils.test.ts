1811738d2fe7c18223272a81cd1e780dc3a6abcb
Michael Myers <michaelmyers@users.noreply.github.com>
2022-05-11 22:26:38 -0400

    fix: Attempt at addressing multi-line markdown bolding (#366)

--- src/__test__/utils.test.ts
+++ src/__test__/utils.test.ts
@@ -22,6 +22,6 @@ describe(`#${addMarkdownHighlight.name}()`, () => {
 describe(`#${addMarkdownHighlights.name}()`, () => {
     it("returns the correct value", () => {
         const highlights = [{ beginOffset: 2, endOffset: 3 }, { beginOffset: 6, endOffset: 7 }]
-        expect(addMarkdownHighlights("a b c d e", highlights)).to.equal("a **b** c **d** e")
+        expect(addMarkdownHighlights("a b c d e", highlights)).to.equal("a **b** c **d** e");
     });
 });
