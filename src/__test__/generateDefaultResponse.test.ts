df0dbc2d13c73b2a3409f6afb4c51a442033a972
Michael Myers <michaelmyers@users.noreply.github.com>
2023-06-21 12:48:04 -0400

    feat: Upgrade the default responses (#450)
    * progress
    
    * docs
    
    * chore:done

---
+++ src/__test__/generateDefaultResponse.test.ts
@@ -0,0 +1,353 @@
+/*! Copyright (c) 2023, XAPP AI */
+import { expect } from "chai";
+
+import { Request, Context, List, IntentRequestBuilder, KnowledgeBaseResult } from "stentor";
+import { SESSION_STORAGE_KNOWLEDGE_BASE_RESULT } from "stentor-constants";
+import { ContextBuilder } from "stentor-context";
+import { isList } from "stentor-response";
+import { generateDefaultResponse } from "../generateDefaultResponse";
+
+
+// We want to test the following channels:
+// - Intelligent Search
+// - Chat Widget
+// - Google Business Messages
+// With the following scenarios:
+// - RAG, Top Answer, Top FAQ, Suggested, Results Only
+// - Unknown answer
+
+import {
+    //   REQUEST_KB_NO_SUGGEST_OR_FAQ_2,
+    REQUEST_KNOWLEDGEBASE_NO_SUGGEST_OR_FAQ,
+    REQUEST_WITH_GOOD_HIGHLIGHTED_ANSWER,
+    // KB Results
+    // RESULT_WITH_NEWLINES,
+    // RESULT_WITH_RAG_RESULT
+} from "./assets/payloads";
+import { generateResultVariables } from "../generateResultVariables";
+
+describe(`#${generateDefaultResponse.name}()`, () => {
+
+    let request: Request;
+    let context: Context;
+
+    describe(`when parameters are passed`, () => {
+        it("returns correctly", () => {
+
+            request = REQUEST_WITH_GOOD_HIGHLIGHTED_ANSWER;
+
+            context = new ContextBuilder()
+                .withSessionData({
+                    id: "foo",
+                    data: {
+                        [SESSION_STORAGE_KNOWLEDGE_BASE_RESULT]: REQUEST_WITH_GOOD_HIGHLIGHTED_ANSWER.knowledgeBaseResult
+                    }
+                })
+                .build();
+
+            const response = generateDefaultResponse(request, context, {});
+            expect(response).to.exist;
+        });
+    });
+    describe(`for intelligent search channel`, () => {
+        it("returns as expected", () => {
+            request = { ...REQUEST_WITH_GOOD_HIGHLIGHTED_ANSWER };
+            request.channel = "intelligent-search";
+
+            const sessionVariables = generateResultVariables(request.rawQuery, REQUEST_WITH_GOOD_HIGHLIGHTED_ANSWER.knowledgeBaseResult, {});
+
+            context = new ContextBuilder()
+                .withSessionData({
+                    id: "foo",
+                    data: {
+                        [SESSION_STORAGE_KNOWLEDGE_BASE_RESULT]: REQUEST_WITH_GOOD_HIGHLIGHTED_ANSWER.knowledgeBaseResult,
+                        ...sessionVariables
+                    }
+                })
+                .build();
+
+            const response = generateDefaultResponse(request, context, {});
+
+            expect(response).to.exist;
+            expect(response.displays).to.have.length(1);
+            const list = response?.displays[0];
+            expect(list).to.exist;
+            expect(isList(list)).to.be.true;
+            if (isList(list)) {
+                expect((list as List).type).to.equal("LIST");
+                const items = list.items;
+                expect(items).to.have.length(6);
+                const item = list.items[0];
+                expect(item.title).to.equal("Bonds | Investor.gov");
+                expect(item.url).to.include("https://www.investor.gov/introduction");
+                expect(item.description).to.include(" To sell an older bond with a lower interest rate")
+            }
+
+            expect(typeof response.outputSpeech).to.equal("object");
+            expect(response.tag).to.equal("KB_SUGGESTED_ANSWER");
+
+            if (typeof response.outputSpeech === "object") {
+                expect(response.outputSpeech.displayText).to.include("Suggested Answer");
+            }
+        });
+        describe("for results only", () => {
+            it("returns as expected", () => {
+
+                request = { ...REQUEST_KNOWLEDGEBASE_NO_SUGGEST_OR_FAQ };
+                request.channel = "intelligent-search";
+
+                const sessionVariables = generateResultVariables(request.rawQuery, REQUEST_KNOWLEDGEBASE_NO_SUGGEST_OR_FAQ.knowledgeBaseResult, {});
+
+                context = new ContextBuilder()
+                    .withSessionData({
+                        id: "foo",
+                        data: {
+                            [SESSION_STORAGE_KNOWLEDGE_BASE_RESULT]: REQUEST_KNOWLEDGEBASE_NO_SUGGEST_OR_FAQ.knowledgeBaseResult,
+                            ...sessionVariables
+                        }
+                    })
+                    .build();
+
+                const response = generateDefaultResponse(request, context, {
+                    search: {
+                        numberOfResults: 2
+                    }
+                });
+                expect(response).to.exist;
+                expect(response.tag).to.equal("KB_LIST_OF_RESULTS");
+
+                expect(response.outputSpeech).to.deep.equal({ displayText: '', ssml: '', suggestions: [] });
+
+                expect(response.displays).to.have.length(1);
+                const list = response?.displays[0];
+                expect(list).to.exist;
+                expect(isList(list)).to.be.true;
+                if (isList(list)) {
+                    expect((list as List).type).to.equal("LIST");
+                    const items = list.items;
+                    expect(items).to.have.length(2);
+                    const item = list.items[0];
+                    expect(item.title).to.equal("Moving from an Apartment to a House Checklist | Travelers Insurance");
+                    expect(item.url).to.include("https://www.travelers.com/resources");
+                    expect(item.description).to.include("Tips for Moving With a Pet")
+                }
+            });
+        });
+        describe("for no results", () => {
+            it("returns as expected", () => {
+                request = { ...REQUEST_WITH_GOOD_HIGHLIGHTED_ANSWER };
+                request.channel = "intelligent-search";
+
+                const sessionVariables = generateResultVariables(request.rawQuery, {}, {});
+
+                context = new ContextBuilder()
+                    .withSessionData({
+                        id: "foo",
+                        data: {
+                            [SESSION_STORAGE_KNOWLEDGE_BASE_RESULT]: {},
+                            ...sessionVariables
+                        }
+                    })
+                    .build();
+
+                const response = generateDefaultResponse(request, context, {});
+
+                expect(response).to.exist;
+                expect(response.displays).to.have.length(0);
+
+                expect(typeof response.outputSpeech).to.equal("object");
+                expect(response.tag).to.equal("KB_NO_ANSWER");
+
+                if (typeof response.outputSpeech === "object") {
+                    expect(response.outputSpeech.displayText).to.include("No Results");
+                }
+            });
+        });
+    });
+    describe("for the chat channel", () => {
+        it("returns as expected", () => {
+            request = REQUEST_WITH_GOOD_HIGHLIGHTED_ANSWER;
+
+            const sessionVariables = generateResultVariables(request.rawQuery, REQUEST_WITH_GOOD_HIGHLIGHTED_ANSWER.knowledgeBaseResult, {});
+
+            context = new ContextBuilder()
+                .withSessionData({
+                    id: "foo",
+                    data: {
+                        [SESSION_STORAGE_KNOWLEDGE_BASE_RESULT]: REQUEST_WITH_GOOD_HIGHLIGHTED_ANSWER.knowledgeBaseResult,
+                        ...sessionVariables
+                    }
+                })
+                .build();
+
+            const response = generateDefaultResponse(request, context, {});
+
+            expect(response).to.exist;
+            expect(response.tag).to.equal("KB_SUGGESTED_ANSWER");
+            expect(response.displays).to.have.length(0);
+
+
+            expect(typeof response.outputSpeech).to.equal("object");
+            if (typeof response.outputSpeech === "object") {
+                expect(response.outputSpeech.displayText).to.include("Any other questions?");
+            }
+
+            expect(typeof response.reprompt).to.equal("object");
+            if (typeof response.reprompt === "object") {
+                expect(response.reprompt.displayText).to.include("Any other questions?");
+            }
+        });
+        describe("with general knowledge answer", () => {
+            it("returns as expected", () => {
+
+                const kb: KnowledgeBaseResult = {
+                    generated: [
+                        {
+                            hasAnswer: true,
+                            generated: "This is the answer",
+                            document: "This is the answer",
+                            uri: "https://xapp.ai",
+                            type: "general-knowledge"
+                        }
+                    ]
+                }
+
+                request = new IntentRequestBuilder().withRawQuery("what is the answer").withIntentId("OCSearch").withKnowledgeBaseResult(kb).build();
+
+                const sessionVariables = generateResultVariables(request.rawQuery, kb, {});
+
+                context = new ContextBuilder()
+                    .withSessionData({
+                        id: "foo",
+                        data: {
+                            [SESSION_STORAGE_KNOWLEDGE_BASE_RESULT]: kb,
+                            ...sessionVariables
+                        }
+                    })
+                    .build();
+
+                const response = generateDefaultResponse(request, context, {
+                    chat: {
+                        followUp: "",
+                        suggestionChips: [{
+                            title: "Suggestion One"
+                        }]
+                    }
+                });
+
+                expect(response).to.exist;
+                expect(response.tag).to.equal("KB_GENERAL_KNOWLEDGE");
+                expect(response.displays).to.have.length(0);
+
+                expect(typeof response.outputSpeech).to.equal("object");
+                if (typeof response.outputSpeech === "object") {
+                    expect(response.outputSpeech.displayText).to.equal("This is the answer");
+
+                    expect(response.outputSpeech.suggestions).to.have.length(1);
+                    expect(response.outputSpeech.suggestions[0]).to.deep.equal({ title: "Suggestion One" });
+                }
+
+                expect(typeof response.reprompt).to.equal("object");
+                if (typeof response.reprompt === "object") {
+                    expect(response.reprompt.displayText).to.equal("");
+                }
+            });
+        });
+        describe("with general knowledge no answer", () => {
+            it("returns as expected", () => {
+
+                const kb: KnowledgeBaseResult = {
+                    generated: [
+                        {
+                            hasAnswer: false,
+                            generated: "This is not the answer",
+                            document: "This is not the answer",
+                            uri: "https://xapp.ai",
+                            type: "general-knowledge"
+                        }
+                    ]
+                }
+
+                request = new IntentRequestBuilder().withRawQuery("what is the answer").withIntentId("OCSearch").withKnowledgeBaseResult(kb).build();
+
+                const sessionVariables = generateResultVariables(request.rawQuery, kb, {});
+
+                context = new ContextBuilder()
+                    .withSessionData({
+                        id: "foo",
+                        data: {
+                            [SESSION_STORAGE_KNOWLEDGE_BASE_RESULT]: kb,
+                            ...sessionVariables
+                        }
+                    })
+                    .build();
+
+                const response = generateDefaultResponse(request, context, {
+                    chat: {
+                        followUp: "Ask something else?",
+                        suggestionChips: []
+                    }
+                });
+
+                expect(response).to.exist;
+                expect(response.tag).to.equal("KB_GENERATED_NO_ANSWER");
+                expect(response.displays).to.have.length(0);
+
+                expect(typeof response.outputSpeech).to.equal("object");
+                if (typeof response.outputSpeech === "object") {
+                    expect(response.outputSpeech.displayText).to.equal("This is not the answer\n\nAsk something else?");
+                    expect(response.outputSpeech.suggestions).to.have.length(0);
+                }
+
+                expect(typeof response.reprompt).to.equal("object");
+                if (typeof response.reprompt === "object") {
+                    expect(response.reprompt.displayText).to.equal("Ask something else?");
+                }
+            });
+        });
+        describe("for just list of results", () => {
+            it("returns as expected", () => {
+                request = REQUEST_KNOWLEDGEBASE_NO_SUGGEST_OR_FAQ;
+
+                const sessionVariables = generateResultVariables(request.rawQuery, REQUEST_KNOWLEDGEBASE_NO_SUGGEST_OR_FAQ.knowledgeBaseResult, {});
+
+                context = new ContextBuilder()
+                    .withSessionData({
+                        id: "foo",
+                        data: {
+                            [SESSION_STORAGE_KNOWLEDGE_BASE_RESULT]: REQUEST_KNOWLEDGEBASE_NO_SUGGEST_OR_FAQ.knowledgeBaseResult,
+                            ...sessionVariables
+                        }
+                    })
+                    .build();
+
+                const response = generateDefaultResponse(request, context, {});
+
+                expect(response).to.exist;
+                expect(response.tag).to.equal("KB_LIST_OF_RESULTS");
+                expect(response.displays).to.have.length(1);
+
+                const list = response?.displays[0];
+                expect(list).to.exist;
+                expect(isList(list)).to.be.true;
+                if (isList(list)) {
+                    expect((list as List).type).to.equal("LIST");
+                    const items = list.items;
+                    expect(items).to.have.length(3);
+                    const item = list.items[0];
+                    expect(item.title).to.equal("Moving from an Apartment to a House Checklist | Travelers Insurance");
+                }
+
+                expect(typeof response.outputSpeech).to.equal("object");
+                if (typeof response.outputSpeech === "object") {
+                    expect(response.outputSpeech.displayText).to.include("Any other questions?");
+                }
+
+                expect(typeof response.reprompt).to.equal("object");
+                if (typeof response.reprompt === "object") {
+                    expect(response.reprompt.displayText).to.include("Any other questions?");
+                }
+            });
+        });
+    });
+});
---
+++ src/generateDefaultResponse.ts
@@ -0,0 +1,261 @@
+/*! Copyright (c) 2023, XAPP AI */
+import { Context, Request, Response, existsAndNotEmpty, List, ListItem } from "stentor";
+import { SuggestionObjectTypes } from "stentor-models";
+
+import { QuestionAnsweringData } from "./QuestionAnsweringHandler";
+import { ResultVariableInformation, ResultVariableFAQInformation, ResultVariableListItem } from "./generateResultVariables";
+
+
+/**
+ * Converts a search results to a List display
+ */
+function generateList(search: ResultVariableListItem[], total: number): List {
+
+    const items: ListItem[] = search.slice(0, total).map((result, index) => {
+        const item: ListItem = {
+            token: `result-${index}`,
+            title: `${result.title}`,
+            description: `${result.document}`
+        };
+        if (result.source) {
+            item.url = result.source
+        }
+
+        return item;
+    });
+
+    return {
+        type: "LIST",
+        title: "Results",
+        items
+    };
+}
+
+/**
+ * Generates default response based on the request and the context.
+ * 
+ * @param request 
+ * @param context 
+ * @param data 
+ * @returns 
+ */
+export function generateDefaultResponse(request: Request, context: Context, data: Pick<QuestionAnsweringData, "chat" | "search">): Response {
+
+    const response: Response = {
+        outputSpeech: {
+            displayText: "",
+            ssml: "",
+            suggestions: []
+        },
+        reprompt: {
+            displayText: "",
+            ssml: ""
+        },
+        displays: []
+    }
+
+    // We want to know the channel
+    const channel: string = request.channel;
+
+
+    const GENERATED_NO_ANSWER: ResultVariableInformation = context.session.get('GENERATED_NO_ANSWER');
+
+    //  Search Results
+    const SEARCH: ResultVariableListItem[] = context.session.get("SEARCH_RESULTS")
+
+    // FAQ
+    const FAQ: ResultVariableFAQInformation = context.session.get("TOP_FAQ");
+
+    // Suggested
+    const SUGGESTED: ResultVariableInformation = context.session.get("SUGGESTED_ANSWER");
+
+    // General Knowledge
+    const GENERAL_KNOWLEDGE: ResultVariableInformation = context.session.get("GENERAL_KNOWLEDGE");
+
+    // Top Answer / RAG
+    const AI_ANSWER: ResultVariableInformation = context.session.get("RAG_RESULT") || context.session.get("TOP_ANSWER");
+
+    let label: string;
+    let displayAnswer: string;
+    let ssmlAnswer: string;
+    let tag: string;
+    const suggestions: SuggestionObjectTypes[] = [];
+
+    if (channel === "intelligent-search") {
+
+        const { topLabels, numberOfResults } = data?.search || {};
+
+        // no reprompt on intelligent-search
+        delete response.reprompt;
+
+        if (AI_ANSWER) {
+            label = topLabels?.AI_ANSWER || "AI Answer";
+            displayAnswer = `${AI_ANSWER.markdownText}`
+            ssmlAnswer = `${AI_ANSWER.text}`;
+            tag = !!context.session.get("RAG_RESULT") ? `KB_RAG` : `KB_TOP_ANSWER`;
+            if (AI_ANSWER.source) {
+                suggestions.push({
+                    title: "Read More",
+                    url: AI_ANSWER.source
+                });
+            }
+        } else if (GENERAL_KNOWLEDGE) {
+            label = topLabels?.GENERAL_KNOWLEDGE || "General Knowledge";
+            displayAnswer = `${GENERAL_KNOWLEDGE.markdownText}`
+            ssmlAnswer = `${GENERAL_KNOWLEDGE.text}`;
+            tag = `KB_GENERAL_KNOWLEDGE`;
+            if (GENERAL_KNOWLEDGE.source) {
+                suggestions.push({
+                    title: "Read More",
+                    url: GENERAL_KNOWLEDGE.source
+                });
+            }
+        } else if (SUGGESTED) {
+            label = topLabels?.SUGGESTED || "Suggested Answer";
+            displayAnswer = `${SUGGESTED.markdownText}`
+            ssmlAnswer = `${SUGGESTED.text}`;
+            tag = `KB_SUGGESTED_ANSWER`;
+
+            if (SUGGESTED.source) {
+                suggestions.push({
+                    title: "Read More",
+                    url: SUGGESTED.source
+                });
+            }
+        } else if (FAQ) {
+            label = topLabels?.FAQ || "FAQ";
+            displayAnswer = `${FAQ.markdownText}`
+            ssmlAnswer = `${FAQ.text}`;
+            tag = `KB_TOP_FAQ`;
+            if (FAQ.source) {
+                suggestions.push({
+                    title: "Read More",
+                    url: FAQ.source
+                });
+            }
+        }
+
+        if (displayAnswer) {
+            response.outputSpeech = {
+                displayText: `${label}\n${displayAnswer}`,
+                ssml: `<speak>${ssmlAnswer}</speak>`,
+                suggestions
+            }
+            response.tag = tag;
+        }
+
+        if (existsAndNotEmpty(SEARCH)) {
+
+            const trimTo: number = typeof numberOfResults === "number" && numberOfResults > 0 ? numberOfResults : 6
+
+            response.displays.push(generateList(SEARCH, trimTo));
+
+            if (!tag) {
+                response.tag = "KB_LIST_OF_RESULTS";
+            }
+        }
+        // No answer scenario, no displayAnswer and no list of results
+
+        if (!displayAnswer && !existsAndNotEmpty(SEARCH)) {
+            // Nothing at all
+            response.outputSpeech = {
+                displayText: `No Results`,
+                ssml: `<speak>No Results</speak>`
+            }
+            response.tag = "KB_NO_ANSWER";
+        }
+
+    } else {
+        /**
+         * DEFAULT PATH
+         * 
+         * -- For chat widget, console, google business messages
+         * -- NOTE: We also check for voice devices and for those, we will NOT display lists
+         */
+
+        const voiceDevice = request?.device?.canSpeak;
+
+        const suggestionChips = data?.chat?.suggestionChips || [];
+
+        const followUp = typeof data?.chat?.followUp === "string" ? data.chat.followUp : "Any other questions?";
+
+        if (AI_ANSWER) {
+            displayAnswer = `${AI_ANSWER.markdownText}\n\n${followUp}`;
+            ssmlAnswer = `${AI_ANSWER.text} ${followUp}`;
+            tag = !!context.session.get("RAG_RESULT") ? `KB_RAG` : `KB_TOP_ANSWER`;
+            if (AI_ANSWER.source) {
+                suggestions.push({
+                    title: "Read More",
+                    url: AI_ANSWER.source
+                });
+            }
+        } else if (FAQ) {
+            displayAnswer = `${FAQ.markdownText}\n\n${followUp}`
+            ssmlAnswer = `${FAQ.text} ${followUp}`;
+            tag = `KB_TOP_FAQ`;
+            if (FAQ.source) {
+                suggestions.push({
+                    title: "Read More",
+                    url: FAQ.source
+                });
+            }
+        } else if (GENERAL_KNOWLEDGE) {
+            displayAnswer = `${GENERAL_KNOWLEDGE.markdownText}\n\n${followUp}`
+            ssmlAnswer = `${GENERAL_KNOWLEDGE.text} ${followUp}`;
+            tag = `KB_GENERAL_KNOWLEDGE`;
+            if (GENERAL_KNOWLEDGE.source) {
+                suggestions.push({
+                    title: "Read More",
+                    url: GENERAL_KNOWLEDGE.source
+                });
+            }
+        } else if (SUGGESTED && !voiceDevice) {
+            displayAnswer = `Here is what I found...\n"${SUGGESTED.markdownText}"\n\n${followUp}`
+            ssmlAnswer = `${SUGGESTED.text} ${followUp}`;
+            tag = `KB_SUGGESTED_ANSWER`;
+            if (SUGGESTED.source) {
+                suggestions.push({
+                    title: "Read More",
+                    url: SUGGESTED.source
+                });
+            }
+        } else if (GENERATED_NO_ANSWER) {
+            displayAnswer = `${GENERATED_NO_ANSWER.markdownText}\n\n${followUp}`
+            ssmlAnswer = `${GENERATED_NO_ANSWER.text} ${followUp}`;
+            tag = `KB_GENERATED_NO_ANSWER`;
+
+            // two possibilities here, if we have search or not
+            if (existsAndNotEmpty(SEARCH) && !voiceDevice) {
+                response.displays.push(generateList(SEARCH, 3));
+            }
+        } else if (existsAndNotEmpty(SEARCH) && !voiceDevice) {
+            displayAnswer = `See if below will help answer your question. ${followUp}`
+            ssmlAnswer = `See if below will help answer your question. ${followUp}`;
+            tag = `KB_LIST_OF_RESULTS`;
+            response.displays.push(generateList(SEARCH, 3));
+        } else {
+            displayAnswer = `I'm sorry, I don't know that one. ${followUp}`
+            ssmlAnswer = `I'm sorry, I don't know that one. ${followUp}`;
+            tag = `KB_NO_ANSWER`;
+        }
+
+        if (displayAnswer) {
+            // Add the suggestions
+            suggestions.push(...suggestionChips);
+            response.outputSpeech = {
+                displayText: displayAnswer.trim(),
+                ssml: `<speak>${ssmlAnswer.trim()}</speak>`,
+                suggestions
+            }
+
+            response.reprompt = {
+                displayText: `${followUp}`,
+                ssml: `<speak>${followUp}</speak>`
+            }
+
+            response.tag = tag;
+        }
+    }
+
+    return response;
+}
